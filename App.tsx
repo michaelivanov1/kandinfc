ef76669f3e2936104cec36a275aa56f3e4e1f84a
Michael Ivanov <michaelivanov153@gmail.com>
2025-07-19 18:22:41 -0400

    added readnfctag button to test

--- App.tsx
+++ App.tsx
@@ -1,13 +1,28 @@
 import React, { JSX } from 'react';
-import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import { SafeAreaView, Text, StyleSheet, Button } from 'react-native';
+import NfcManager, { NfcTech } from 'react-native-nfc-manager';
 
 const App = (): JSX.Element => {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.text}>kandi app</Text>
+      <Button onPress={readNfcTag} title="read nfc tag" />
     </SafeAreaView>
   );
 };
+
+async function readNfcTag() {
+  try {
+    await NfcManager.requestTechnology(NfcTech.Ndef);
+    const tag = await NfcManager.getTag();
+    console.log("Tag ID:", tag?.id);
+    // eventually send ID to backend
+  } catch (ex) {
+    console.warn('NFC error', ex);
+  } finally {
+    NfcManager.cancelTechnologyRequest();
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
