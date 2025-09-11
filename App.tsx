dd5981f90d4339dff56319b52de97ed68bb63201
Michael <michaelivanov153@gmail.com>
2025-09-11 15:37:46 -0400

    added run instructions in app.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
+// run:
+// npx react-native run-android
+
 import React from 'react';
 import AppNavigator from './android/src/navigation/AppNavigator';
 
 export default function App() {
   return <AppNavigator />;
 }
+
+
