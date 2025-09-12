980bc10f38cb4885245c3bbaec72b5fd4bdc8983
Michael Ivanov <michaelivanov153@gmail.com>
2025-09-12 19:08:52 -0400

    added metro errors comment for fixing

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@
 // npx react-native run-android
 // npx react-native log-android
 
+// for metro errors on new system after git pull:
+// npx react-native start --reset-cache
+
 import React from 'react';
 import AppNavigator from './android/src/navigation/AppNavigator';
 
