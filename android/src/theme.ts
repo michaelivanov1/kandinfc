034f0594739653bacbcb6a93f99e3c3de8dda3cf
Michael Ivanov <michaelivanov153@gmail.com>
2025-10-03 00:26:15 -0400

    added primary app themes

---
+++ android/src/theme.ts
@@ -0,0 +1,25 @@
+// src/theme.ts
+export const Colors = {
+    background: '#fff',
+    text: '#333',
+    heading: '#000',
+    muted: '#666',
+    primary: '#9e0437ff',
+    grayLight: '#f8f8f8',
+};
+
+export const FontSizes = {
+    title: 22,
+    subtitle: 16,
+    section: 16,
+    body: 14,
+    small: 12,
+};
+
+export const Spacing = {
+    xs: 4,
+    sm: 8,
+    md: 12,
+    lg: 20,
+    xl: 32,
+};
