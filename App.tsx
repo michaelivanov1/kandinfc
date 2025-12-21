8ea55c57a18c57b2fcceebd980e7f0af175b9968
Michael Ivanov <michaelivanov153@gmail.com>
2025-12-21 15:51:24 -0500

    removed refundant firebase app initialization

--- App.tsx
+++ App.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
-import { firebase } from '@react-native-firebase/app';
-
-// Initialize Firebase once
-if (!firebase.apps.length) {
-  firebase.initializeApp();
-}
 
 export default function App() {
   return <AppNavigator />;
