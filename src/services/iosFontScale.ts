3ac21e5a7568a5ac917840c011180da7b3252efe
Michael Ivanov <michaelivanov153@gmail.com>
2025-12-22 14:40:44 -0500

    ios: changed font scaling to 1.4

--- src/services/iosFontScale.ts
+++ src/services/iosFontScale.ts
@@ -1,6 +1,6 @@
 import { Platform, PixelRatio } from 'react-native';
 
-const IOS_FONT_SCALE = 1.; // scale factor for iOS font sizing
+const IOS_FONT_SCALE = 1.4; // scale factor for iOS font sizing
 
 export const iosFontScale = (size: number) => {
     const scaled = Platform.OS === 'ios' ? size * IOS_FONT_SCALE : size;
